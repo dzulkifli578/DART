1ea02f754d7295d7008f535021289976dcd200b0
dzulkifli578 <dzulkiflianwar2@gmail.com>
2024-04-26 08:41:39 +0700

    Adding 'Comments.dart'

---
+++ 1. Syntax Basics/Comments.dart
@@ -0,0 +1,21 @@
+void main() {
+  // This is a single-line comment.
+  // Single-line comments are preceded by two forward slashes.
+  // It is used to add explanatory notes or annotations to the code.
+  // They are ignored by the Dart compiler.
+
+  /*
+   * This is a multi-line comment.
+   * Multi-line comments start with /* and end with * /.
+   * It can span across multiple lines and is useful for longer explanations.
+   * Everything between them is ignored by the Dart compiler.
+   */
+  */
+
+  /// This is a documentation comment.
+  /// It is used to generate documentation for libraries, classes, functions, and variables.
+  /// Documentation comments start with three forward slashes.
+  
+  // Comments are essential for documenting code and making it easier to understand.
+  // They help other developers (and yourself) comprehend the purpose and functionality of the code.
+}
